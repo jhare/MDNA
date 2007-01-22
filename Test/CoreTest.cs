1967df961faa0ac42c50fb4d8020a04d67c97923
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-01-22 20:18:22 +0000

    Adding placeholder files for Core and its test.

---
+++ Test/CoreTest.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit.Framework;
+
+namespace mdna.Test
+{
+	/// <summary>
+	/// Unit tests for application core.
+	/// </summary>
+	public class CoreTest
+	{
+		public CoreTest()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
