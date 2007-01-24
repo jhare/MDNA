2e3faea38ccb8a6092f9f3184a1e42cfb076c85e
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-01-24 21:31:22 +0000

    Okay, apparently VS creates namespaces for each folder, this is unacceptable, and I'm fixing it.

---
+++ Core.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace mdna.Source.Core
+{
+	/// <summary>
+	/// Core contains all of the default general procedures for mdna.
+	/// </summary>
+	public class Core
+	{
+		public Core()
+		{
+			
+		}
+	}
+}
