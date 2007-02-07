486317d1d8bf0d2d03bf7e84d8c3844c729c0590
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-02-07 19:51:50 +0000

    Added our toolbar and a "batch" command that is a part of that toolbar. More to come. The MDNA toolbar is registered as a premier toolbar.

---
+++ DoAllCmd.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+using ESRI.ArcGIS.Utility.CATIDs;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.SystemUI;
+
+namespace mdna
+{
+	/// <summary>
+	/// This class implements the ICommand interface in order to offer
+	/// a "do all" command to the user that does all replacements and
+	/// addition of notation that it knows how to do at that moment.
+	/// </summary>
+	public class DoAllCmd : ICommand
+	{
+		#region Data Members
+		private IApplication m_app;
+		private bool m_enabled;
+		#endregion
+
+		public DoAllCmd()
+		{
+			m_app = null;
+			m_enabled = false;
+		}
+
+		#region ICommand Members
+
+		public void OnClick()
+		{
+			MessageBox.Show( "Not yet implemented." );
+		}
+
+		public string Message
+		{
+			get
+			{
+				return( "Perform all substitution operations." );
+			}
+		}
+
+		public int Bitmap
+		{
+			get
+			{
+				return 0;
+			}
+		}
+
+		public void OnCreate(object hook)
+		{
+			if( hook != null && hook is IMxApplication )
+			{
+				m_app = (IApplication)hook;
+				m_enabled = true;
+			}
+			else
+			{
+				m_enabled = false;
+			}
+		}
+
+		public string Caption
+		{
+			get
+			{
+				return( "Do All" );
+			}
+		}
+
+		public string Tooltip
+		{
+			get
+			{
+				return( "This command replaces all replaceable elements in the map." );
+			}
+		}
+
+		public int HelpContextID
+		{
+			get
+			{
+				return 0;
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return( "DoAll" );
+			}
+		}
+
+		public bool Checked
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public bool Enabled
+		{
+			get
+			{
+				return( m_enabled );
+			}
+		}
+
+		public string HelpFile
+		{
+			get
+			{
+				return null;
+			}
+		}
+
+		public string Category
+		{
+			get
+			{
+				return( "MDNA" );
+			}
+		}
+
+		#endregion
+
+		#region "Component Category Registration"
+		[ComRegisterFunction()]
+		static void Reg(string regKey)
+		{
+				MxCommands.Register(regKey);
+		}
+
+		[ComUnregisterFunction()]
+		static void Unreg(string regKey)
+		{
+				MxCommands.Unregister(regKey);
+		}
+		#endregion
+
+	}
+}
---
+++ MDNAToolbar.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.Utility.CATIDs;
+
+namespace mdna
+{
+	/// <summary>
+	/// Summary description for MDNAToolbar.
+	/// </summary>
+	public class MDNAToolbar : IToolBarDef
+	{
+		public MDNAToolbar()
+		{
+			
+		}
+
+		#region IToolBarDef Members
+
+		public void GetItemInfo(int pos, IItemDef itemDef)
+		{
+			// For now we only have the "batch" command.
+
+			switch( pos )
+			{
+				case 0:
+					itemDef.ID = "mdna.DoAllCmd";
+					itemDef.Group = false;
+					break;
+			}
+		}
+
+		public string Caption
+		{
+			get
+			{
+				return( "MDNA Toolbar" );
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return( "MDNA Toolbar" );
+			}
+		}
+
+		public int ItemCount
+		{
+			get
+			{
+				return( 1 );
+			}
+		}
+
+		#endregion
+
+		#region "Component Category Registration"
+		[ComRegisterFunction()]
+		static void Reg(string regKey)
+		{
+				MxCommandBars.Register(regKey);
+				string theGUID = regKey.Substring(regKey.LastIndexOf(@"\") + 1);
+				string keyStr = @"Software\ESRI\ArcCatalog\Settings\PremierToolbars\";
+				Microsoft.Win32.RegistryKey premierRegKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(keyStr, true);
+				if (premierRegKey == null)
+				{
+					premierRegKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(keyStr);
+				}
+				if (premierRegKey != null)
+				{
+					premierRegKey.CreateSubKey(theGUID);
+					premierRegKey.Close();
+				}
+		}
+
+		[ComUnregisterFunction()]
+		static void Unreg(string regKey)
+		{
+				MxCommandBars.Unregister(regKey);
+				string theGUID = regKey.Substring(regKey.LastIndexOf(@"\") + 1);
+				string keyStr = @"Software\ESRI\ArcCatalog\Settings\PremierToolbars\";
+				Microsoft.Win32.RegistryKey premierRegKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(keyStr, true);
+				if (premierRegKey != null)
+				{
+					premierRegKey.DeleteSubKey(theGUID);
+					premierRegKey.Close();
+				}
+		}
+		#endregion
+
+
+	}
+}
