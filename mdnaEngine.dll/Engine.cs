31af11436db405718142e8da87cabedc144cfe52
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-02-08 01:37:06 +0000

    This subproject deals with the technicalities of item replacement, etc. i.e. Dealing with ArcMap's arcobject interface not having to do with the interface of MDNA itself.

---
+++ mdnaEngine.dll/Engine.cs
@@ -0,0 +1,25 @@
+using System;
+using ESRI.ArcGIS.Utility.CATIDs;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.ArcMap;
+using ESRI.ArcGIS.esriSystem;
+
+namespace mdnaEngine.dll
+{
+	/// <summary>
+	/// Summary description for Engine.
+	/// </summary>
+	public class Engine
+	{
+		public Engine()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
