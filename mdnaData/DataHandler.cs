00d28f58afd183c3ad68067b964355d02cbb985f
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-02-08 01:21:53 +0000

    Getting started on designing our data handling side.

---
+++ mdnaData/DataHandler.cs
@@ -0,0 +1,18 @@
+using System;
+using Oracle.DataAccess;
+
+namespace mdnaData
+{
+	/// <summary>
+	/// Summary description for DataHandler.
+	/// </summary>
+	public class DataHandler
+	{
+		public DataHandler()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
