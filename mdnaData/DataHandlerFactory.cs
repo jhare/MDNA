2bcbaa47d85b01c2cc2fa2e6191033d7a17dac48
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-02-08 02:10:06 +0000

    I decided a factory pattern for the data handler is appropriate. We may even add map server support, so a factory is a good thing. ESRI uses the same for their multiple database support. I believe this is smart. I could be wrong.

---
+++ mdnaData/DataHandlerFactory.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace mdnaData
+{
+	/// <summary>
+	/// Summary description for DataHandlerFactory.
+	/// </summary>
+	public class DataHandlerFactory
+	{
+		public DataHandlerFactory()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
---
+++ mdnaData/Tests/DataHandlerFactoryTest.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace mdnaData.Tests
+{
+	/// <summary>
+	/// Summary description for DataHandlerFactoryTest.
+	/// </summary>
+	public class DataHandlerFactoryTest
+	{
+		public DataHandlerFactoryTest()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
