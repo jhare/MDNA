33b176aeca213163b974c25cc9561697ddd1e357
Joseph.Hare <Joseph.Hare@decfc1ef-6a27-0410-9dff-dffed65e58c7>
2007-02-08 01:46:16 +0000

    Okay, everything is finally in order.

---
+++ mdnaEngine/Engine.cs
@@ -0,0 +1,24 @@
+using System;
+
+using ESRI.ArcGIS.Utility.CATIDs;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.SystemUI;
+ 
+namespace mdnaEngine
+{
+	/// <summary>
+	/// Summary description for Engine.
+	/// </summary>
+	public class Engine
+	{
+		public Engine()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
